9ae6816fbe6a6ee9b5e2e1d5310db6469071cbc5
Moonkitten <lauragreer3@gmail.com>
2019-01-13 00:37:12 -0700

    baby steps

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,13 +11,13 @@ var the_game = {
     init_game: function () {
         num_guesses = 0;
         word_dictionary = ["Aladdin", "Mufasa", "Cars", "Jasmin"];
-        random_pick = Math.floor((Math.random() * (this.word_dictionary.length -1)))
+        random_pick = Math.floor((Math.random() * (the_game.word_dictionary.length)));
         this.current_word = this.word_dictionary[random_pick];
         this.display_word ="";
-        for (x = 0; x < this.current_word.length -1; x++) {
+        for (x = 0; x < this.current_word.length; x++) {
             this.display_word += "_"; //for each letter in display word, set to underscore
         }
-        document.getElementById("current_word").innerHTML = display_word;
+        document.getElementById("current_word").innerHTML = this.display_word;
         this.all_guesses = [];
         this.hangman_status = 0;
         /*
@@ -29,8 +29,8 @@ var the_game = {
             hangman_status_5.jpg = hangman head, body, r_arm, l_arm, r_leg
             hangman_status_6.jpg = hangman head, body, r_arm, l_arm, r-leg, l_leg (game over)
         */
-        hangman_status_prefix = "assets/images/hangman_status_";
-        document.getElementById("this_state_img").src = hangman_img_prefix + hangman_status + ".jpg";
+        hangman_img_prefix = "assets/images/hangman_status_";
+        document.getElementById("game_state_img").src = hangman_img_prefix + this.hangman_status + ".jpg";
 
         this.game_state = "new_game"
 
@@ -39,20 +39,30 @@ var the_game = {
             playing
             game_over_win = 0;
             game_over_losses = 0;*/
+    },
+    handle_keypress: function(key) {
+        var keypressed = key.toLowerCase();
+        var temp_current_word = this.current_word.toLowerCase();
+        var guessed_correctly = false;
+        for (x = 0; x < this.current_word.length -1; x++) {
+            if (temp_current_word.charAt(x) === keypressed) {
+                console.log("checking if " + temp_current_word.charAt(x) + " = " + keypressed)
+                guessed_correctly = true;
+                
+            }    
+        }
     }
 
 }
 
 
-
+the_game.init_game();
 document.addEventListener("keypress", function onPress(event) {
     //if (event.key === "z" && event.ctrlKey) {
         // // DO something awesome        
     //}
     //check if letter is in the game.current_word
-    for (x = 0; x < current_word.length -1; x++) {
-        game.display_word.charAt(x).toLower();
-    }
-    
+    console.log("keypressed: " + event.key);
+    the_game.handle_keypress(event.key);
     
 });
